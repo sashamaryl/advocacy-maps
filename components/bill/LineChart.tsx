b2534bf793468611fa70c573d718471fdf11e39a
sashamaryl <sashamaryl@tgmail.com>
2022-11-11 14:46:08 +0000

    adds prelim full coverage regexing

--- components/bill/LineChart.tsx
+++ components/bill/LineChart.tsx
@@ -1,11 +1,20 @@
-import { dimensions } from "analysis/components/GraphUtilities"
 import * as d3 from "d3"
+import { capitalize } from "instantsearch.js/es/lib/utils"
 import { useEffect, useMemo, useRef } from "react"
-import { string } from "yargs"
 import rawdata from "./temp-chart-data.json"
 
 type Stance = "endorse" | "neutral" | "oppose"
 const stances: Stance[] = ["endorse", "neutral", "oppose"]
+
+const height = 192,
+  width = 607,
+  margin = 32,
+  frameHeight = 303
+
+const top = margin,
+  bottom = top + height,
+  left = margin,
+  right = left + width
 
 type TestimonyData = {
   id: number
@@ -14,7 +23,7 @@ type TestimonyData = {
   stanceCounts: any
 }
 
-const colorDict = {
+const colorDict: { [name: string]: string } = {
   endorse: "var(--bs-orange)",
   neutral: "var(--bs-blue)",
   oppose: "var(--bs-green)"
@@ -44,19 +53,11 @@ export const LineChart = ({
     const dates = data.map(d => d.date)
     const dateExtent = d3.extent(dates) as [Date, Date]
 
-    return d3
-      .scaleTime()
-      .domain(dateExtent)
-      .range([dimensions.margin, dimensions.width - dimensions.margin])
+    return d3.scaleTime().domain(dateExtent).range([left, right])
   }, [data])
 
   const yScale = useMemo(() => {
-    return d3
-      .scaleLinear()
-      .domain([0, 60])
-      .range(
-        [dimensions.margin, dimensions.height - dimensions.margin].reverse()
-      )
+    return d3.scaleLinear().domain([0, 60]).range([top, bottom].reverse())
   }, [])
 
   const xAxis = d3.axisBottom(xScale)
@@ -74,84 +75,75 @@ export const LineChart = ({
   useEffect(() => {
     const svg = d3
       .select(ref.current)
-      .attr("height", dimensions.height)
-      .attr("width", dimensions.width)
+      .attr("height", frameHeight)
+      .attr("width", width + margin * 2)
 
-    const oppLinePoints = data.map(d => [
-      xScale(d.date),
-      yScale(d.stanceCounts["oppose"])
-    ]) as [number, number][]
+    stances.forEach(stance => {
+      const points = data.map(d => [
+        xScale(d.date),
+        yScale(d.stanceCounts[stance])
+      ]) as [number, number][]
 
-    const neuLinePoints = data.map(d => [
-      xScale(d.date),
-      yScale(d.stanceCounts["neutral"])
-    ]) as [number, number][]
-
-    const endLinePoints = data.map(d => [
-      xScale(d.date),
-      yScale(d.stanceCounts["endorse"])
-    ]) as [number, number][]
-
-    svg
-      .append("g")
-      .append("path")
-      .attr("d", line(oppLinePoints))
-      .attr("fill", "none")
-      .attr("stroke", d => colorDict["oppose"])
-      .attr("stroke-width", 6)
-
-    svg
-      .append("g")
-      .append("path")
-      .attr("d", line(neuLinePoints))
-      .attr("fill", "none")
-      .attr("stroke", d => colorDict["neutral"])
-      .attr("stroke-width", 6)
-
-    svg
-      .append("g")
-      .append("path")
-      .attr("d", line(endLinePoints))
-      .attr("fill", "none")
-      .attr("stroke", d => colorDict["endorse"])
-      .attr("stroke-width", 6)
+      svg
+        .append("g")
+        .append("path")
+        .attr("d", line(points))
+        .attr("fill", "none")
+        .attr("stroke", d => colorDict[stance])
+        .attr("stroke-width", 3)
+    })
 
     const labelSection = svg
+      .append("g")
       .append("foreignObject")
-      .attr("height", dimensions.height)
-      .attr("width", dimensions.width)
+      .style("height", "30px")
+      .style("width", "320px")
+      .style("x", 320 / 2 + "px")
+      .style("y", height + margin + 30 + "px")
+      .style("transform", `translate(-50% 100% - 30px)`)
       .append("xhtml:div")
-      .attr("height", dimensions.height)
-      .attr("width", dimensions.width)
-      .style("y", dimensions.height - dimensions.margin)
+      .style("height", "100%")
+      .style("width", "100%")
+
+    const labelBox = labelSection
+      .append("xhtml:div")
+      .style("height", "30px")
+      .style("width", "320px")
+
       .style("display", "flex")
       .style("flex-direction", "row")
       .style("justify-content", "space-around")
-      .style("border", "3px solid gold")
 
-    labelSection
+    const labelBoxes = labelBox
       .selectAll("div")
-      .attr("class", "line-labels")
       .data(guideData)
       .enter()
       .append("div")
-      .attr("background-color", "blue")
-      .attr("flex", "1")
-      .append("text")
-      .text(d => `${d[0]} ${d[1]}`)
-      .attr("color", "orange")
+      .attr("class", "line-labels")
+      .style("display", "flex")
+      .style("align-items", "center")
 
-    svg
-      .append("g")
-      .call(xAxis)
-      .attr(
-        "transform",
-        `translate(0 ${dimensions.height - dimensions.margin})`
-      )
-    svg
-      .append("g")
-      .call(yAxis)
-      .attr("transform", `translate(${dimensions.margin} 0)`)
+    labelBoxes
+      .append("text")
+      .style("flex", "1")
+      .style("font-size", 16 + "px")
+      .style("font-weight", 500)
+      .style("text-anchor", "middle")
+      .text(d => `${capitalize(d[0])}`)
+      .style("color", d => colorDict[d[0]])
+
+    labelBoxes
+      .append("text")
+      .style("flex", "1")
+      .style("font-size", 25 + "px")
+      .style("font-weight", 500)
+      .style("text-anchor", "middle")
+      .text(d => ` ${d[1]}`)
+      .style("color", "var(--bs-black)")
+      .style("", "middle")
+
+    svg.append("g").call(xAxis).attr("transform", `translate(0 ${bottom})`)
+    svg.append("g").call(yAxis).attr("transform", `translate(${left} 0)`)
   }, [data, guideData, line, xAxis, xScale, yAxis, yScale])
 
   return <svg ref={ref}></svg>
